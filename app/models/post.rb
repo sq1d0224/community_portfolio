feba5773e68729896f0e1b8bed6fb6bd676a8230
sq1d0224 <isho05010224@gmail.com>
2024-08-25 16:10:23 +0000

    スタイルの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,13 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   
+  # 画像表示メソッド
   def display_image
-    image.variant(resize_to_limit: [300, 300]).processed
+    if image.attached?
+      image.variant(resize_to_limit: [300, 300]).processed
+    else
+      'no_image_photo.jpg' # 画像が添付されていない場合のデフォルト画像
+    end
   end
   
 end
