d458c5ec77301016294ac971a9b8ae68940b3e67
sq1d0224 <isho05010224@gmail.com>
2024-08-31 11:58:18 +0000

    追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def top
     @user = current_user
@@ -11,19 +12,21 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @post = Post.new
     @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(20)
   end
 
   def edit
-    @user = current_user
+    is_matching_login_user
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = current_user
+    is_matching_login_user
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_profile_path, notice: 'プロフィールを更新しました。'
+      redirect_to user_path(@user), notice: 'プロフィールを更新しました。'
     else
       render :edit
     end
@@ -33,5 +36,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :gender, :birthdate, :bio, :profile_image, :remove_profile_image)
+  end
+  
+  def is_matching_login_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path, alert: "アクセス権がありません。"
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,12 @@ class Post < ApplicationRecord
 
   # バリデーション
   validates :title, presence: true, length: { maximum: 100 }
-  validates :description, presence: true, length: { maximum: 10000 }
+  validates :description, presence: true, length: { maximum: 3000 }
   
   # 画像表示メソッド
   def display_image
     if image.attached?
-      image.variant(resize_to_limit: [300, 300]).processed
+      image.variant(resize_to_limit: [800, 800]).processed
     else
       'no_image_photo.jpg' # 画像が添付されていない場合のデフォルト画像
     end
