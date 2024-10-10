fe0d1564e9405f94a58aacce918a6853e1415a66
sq1d0224 <isho05010224@gmail.com>
2024-10-10 04:50:53 +0000

    完了

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+// Remove invalid node properties
+environment.config.set('node', {
+  __dirname: false,
+  __filename: false,
+  global: true
+});
+
 module.exports = environment
